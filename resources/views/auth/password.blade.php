8a5edd093038d4f5098f6f6a8c4ae7e6533ca08f
Yoser Perez <yoserp1@gmail.com>
2020-04-02 09:53:36 -0400

    Desarrollo de interfaz

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,59 +1,20 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <!--
+@extends('layouts.auth')
 
+@section('css_before')
+    <!-- Page JS Plugins CSS -->
 
+@endsection
 
+@section('js_after')
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('/assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <!-- Page JS Code -->
+    <script src="{{ asset('/assets/js/pages/auth_reminder.min.js') }}"></script>
+@endsection
 
+@section('content')
 
-
-        THIS IS THE DEMO VERSION OF Dashmix UI FRAMEWORK
-        COMMENTS ARE REMOVED, CODE IS COMPRESSED AND CODE STYLE IS ALTERED
-
-        IN THE FULL VERSION
-            THE CODE IS INDENTED CORRECTLY AND WELL COMMENTED
-            YOU GET AN EXTENSIVE DOCUMENTATION TO GET YOU STARTED
-            YOU GET THE PHP VERSION (ALONG WITH THE HTML VERSION) WHICH HAS EXTRA FEATURES
-            YOU GET THE LARAVEL STARTER KIT TO HELP YOU BUILD YOUR WEB APP WITH LARAVEL
-            YOU GET THE GET STARTED VERSION WITH RTL SUPPORT
-            YOU GET THE ES6 FILES OF THE UI FRAMEWORK
-            YOU GET THE SASS FILES OF THE UI FRAMEWORK
-            YOU GET A GULPFILE WITH LOTS OF AUTOMATIONS
-            YOU GET ACCESS TO FRAMEWORK UPDATES WITH NEW FEATURES AND PLUGIN UPGRADES
-            WE CAN PROVIDE SUPPORT IF YOU NEED IT :-)
-
-        THANK YOU FOR HAVING A LOOK!
-
-        ** A LICENSE MUST BE ACQUIRED FOR LEGAL USE -> LINK: https://1.envato.market/r6y **
-        ** PLEASE SUPPORT US IF YOU LIKE OUR WORK, IT HELPS US CONTINUE, THANK YOU VERY MUCH! **
-
-
-
-
-
-        -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title>Dashmix - Bootstrap 4 Admin Template &amp; UI Framework | DEMO</title>
-        <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest | This is the demo of Dashmix! You need to purchase a license for legal use! | DEMO">
-        <meta name="author" content="pixelcave">
-        <meta name="robots" content="noindex, nofollow">
-        <meta property="og:title" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework | DEMO">
-        <meta property="og:site_name" content="Dashmix">
-        <meta property="og:description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest | This is the demo of Dashmix! You need to purchase a license for legal use! | DEMO">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="">
-        <meta property="og:image" content="">
-        <link rel="shortcut icon" href="{{ asset('/assets/media/favicons/favicon.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('/assets/media/favicons/favicon-192x192.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/media/favicons/apple-touch-icon-180x180.png') }}">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
-        <link rel="stylesheet" id="css-main" href="{{ asset('/assets/css/dashmix.min-2.3.css') }}">
-</head>
-<body>
-<div id="page-container">
-                <main id="main-container">
+<!-- Page Content -->
 <div class="bg-image" style="background-image: url('{{ asset('/assets/media/photos/photo19@2x.jpg') }}');">
     <div class="row no-gutters bg-gd-sun-op">
         <div class="hero-static col-md-6 d-flex align-items-center bg-white">
@@ -124,11 +85,6 @@
         </div>
     </div>
 </div>
-    </main>
-    </div>
-<script src="{{ asset('/assets/js/dashmix.core.min-2.3.js') }}"></script>
-<script src="{{ asset('/assets/js/dashmix.app.min-2.3.js') }}"></script>
-<script src="{{ asset('/assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('/assets/js/pages/auth_reminder.min.js') }}"></script>
-    </body>
-</html>
+
+<!-- END Page Content -->
+@endsection
