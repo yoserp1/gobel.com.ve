154aec60bc6a34ec3892f14d99d3e4045c366539
Yoser Perez <yoserp1@gmail.com>
2020-04-15 08:11:52 -0400

    modulo de modulos

--- app/Http/Controllers/Cms/moduloController.php
+++ app/Http/Controllers/Cms/moduloController.php
@@ -1,7 +1,15 @@
 <?php
 
 namespace portal\Http\Controllers\Cms;
-
+//*******agregar esta linea******//
+use portal\Models\Cms\tab_item;
+use Validator;
+use View;
+use Auth;
+use DB;
+use Session;
+use Redirect;
+//*******************************//
 use Illuminate\Http\Request;
 
 use portal\Http\Requests;
@@ -9,5 +17,104 @@ use portal\Http\Controllers\Controller;
 
 class moduloController extends Controller
 {
-    //
+    /**
+    * Create a new controller instance.
+    *
+    * @return void
+    */
+    public function __construct()
+    {
+      $this->middleware('auth');
+      $this->middleware('optimizar');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function lista(Request $request)
+    {
+
+        $sortBy = 'id';
+        $orderBy = 'desc';
+        $perPage = 5;
+        $q = null;
+        $desde = null;
+        $hasta = null;
+        $columnas = [
+            ['valor'=>'bnumberdialed', 'texto'=>'Número de Origen'],
+            ['valor'=>'bnumberdialed', 'texto'=>'Número de Destino']
+        ];
+    
+        if ($request->has('orderBy')){
+            $orderBy = $request->query('orderBy');
+        }
+        if ($request->has('sortBy')){
+            $sortBy = $request->query('sortBy');
+            $sortBy = 'id';
+        } 
+        if ($request->has('perPage')){
+            $perPage = $request->query('perPage');
+        } 
+        if ($request->has('q')){
+            $q = $request->query('q');
+        }
+
+        if ($request->has('desde')){
+            $desde = $request->desde;
+        }
+
+        if ($request->has('hasta')){
+            $hasta = $request->hasta;
+        }
+
+        $tab_item = tab_item::select( 'id', 'de_item', 'de_contenido')
+        ->where('in_activo', '=', true)
+        ->search($q, $sortBy)
+        //->fecha($desde, $hasta)
+        ->orderBy($sortBy, $orderBy)
+        ->paginate($perPage);
+
+        return View::make('cms.modulo.lista')->with([
+            'tab_item' => $tab_item,
+            'orderBy' => $orderBy,
+            'sortBy' => $sortBy,
+            'perPage' => $perPage,
+            'columnas' => $columnas,
+            'q' => $q,
+            'desde' => $desde,
+            'hasta' => $hasta
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function nuevo()
+    {
+        $data = array( "id" => null);
+
+        return View::make('cms.modulo.nuevo')->with([
+            'data'  => $data
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function editar($id)
+    {
+        $data = tab_item::select( 'id', 'de_item', 'de_contenido')
+        ->where('id', '=', $id)
+        ->first();
+
+        return View::make('cms.modulo.editar')->with([
+            'data'  => $data
+        ]);
+    }
 }
---
+++ app/Models/Cms/tab_item.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace portal\Models\Cms;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tab_item extends Model
+{
+    //Nombre de la conexion que utitlizara este modelo
+    protected $connection= 'cms';
+
+    //Todos los modelos deben extender la clase Eloquent
+    protected $table = 'tab_item';
+
+    public function scopeSearch($query, $q, $sortBy)
+    {
+      switch ($sortBy) {
+          case 'id':
+              return $query->where('de_item', 'ILIKE', "%{$q}%");
+          break;
+            default:
+              return $query;
+          break;
+      }
+    }
+}
