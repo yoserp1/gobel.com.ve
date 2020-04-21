a5de28abdc74d380f1756af41b7153ed05be2634
Yoser Perez <yoserp1@gmail.com>
2020-04-21 15:00:06 -0400

    boton bandeja

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,23 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>Dashmix - Bootstrap 4 Admin Template &amp; UI Framework</title>
+        <title>CMS|mix</title>
 
-        <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-        <meta name="author" content="pixelcave">
+        <meta name="description" content="CMS|mix">
+        <meta name="author" content="yoserp1">
         <meta name="robots" content="noindex, nofollow">
-        <meta property="og:title" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework | DEMO">
-        <meta property="og:site_name" content="Dashmix">
-        <meta property="og:description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest | This is the demo of Dashmix! You need to purchase a license for legal use! | DEMO">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="">
-        <meta property="og:image" content="">
+        <meta property="og:title" content="CMS|mix">
+        <meta property="og:site_name" content="CMS|mix">
+        <meta property="og:description" content="CMS|mix">
+        <meta property="og:type" content="CMS|mix">
+        <meta property="og:url" content="CMS|mix">
+        <meta property="og:image" content="CMS|mix">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Icons -->
-        <link rel="shortcut icon" href="{{ asset('assets/media/favicons/favicon.png') }}">
+        <link rel="shortcut icon" href="{{ asset('/images/favicon.ico') }}" type="image/ico">
         <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('assets/media/favicons/favicon-192x192.png') }}">
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/media/favicons/apple-touch-icon-180x180.png') }}">
 
