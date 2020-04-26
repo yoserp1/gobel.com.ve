58366f897e9fa6e885b220c33f977c4bd687d9e0
Yoser Perez <yoserp1@gmail.com>
2020-04-26 10:27:12 -0400

    Modulo articulo

--- app/Http/Controllers/Cms/articuloController.php
+++ app/Http/Controllers/Cms/articuloController.php
@@ -2,6 +2,7 @@
 
 namespace portal\Http\Controllers\Cms;
 //*******agregar esta linea******//
+use portal\Models\Cms\tab_articulo;
 use Validator;
 use View;
 use Auth;
@@ -34,6 +35,39 @@ class articuloController extends Controller
      */
     public function lista(Request $request)
     {
+      $sortBy = 'id';
+      $orderBy = 'desc';
+      $perPage = 5;
+      $q = null;
+      $columnas = [
+          ['valor'=>'bnumberdialed', 'texto'=>'Número de Origen'],
+          ['valor'=>'bnumberdialed', 'texto'=>'Número de Destino']
+      ];
+  
+      if ($request->has('orderBy')){
+          $orderBy = $request->query('orderBy');
+      }
+      if ($request->has('sortBy')){
+          $sortBy = $request->query('sortBy');
+      } 
+      if ($request->has('perPage')){
+          $perPage = $request->query('perPage');
+      } 
+      if ($request->has('q')){
+          $q = $request->query('q');
+      }
 
+      $tab_articulo = tab_articulo::select( 'id')
+      ->orderBy($sortBy, $orderBy)
+      ->paginate($perPage);
+
+      return View::make('cms.articulo.lista')->with([
+          'tab_articulo' => $tab_articulo,
+          'orderBy' => $orderBy,
+          'sortBy' => $sortBy,
+          'perPage' => $perPage,
+          'columnas' => $columnas,
+          'q' => $q
+      ]);
     }
 }
---
+++ resources/views/cms/articulo/lista.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.dashboard')
+
+@section('css_before')
+    <!-- Page JS Plugins CSS -->
+
+@endsection
+
+@section('js_after')
+    <!-- Page JS Plugins -->
+
+    <!-- Page JS Code -->
+    <script>
+        $('.pagination').addClass('justify-content-end');
+        $('.pagination li').addClass('page-item');
+        $('.pagination li a').addClass('page-link');
+        $('.pagination span').addClass('page-link');
+    </script>
+
+    <script>
+        $('#borrar').on('show.bs.modal', function (event) {
+            $("#borrarForm").attr('action','{{ url('/cms/articulo/eliminar') }}');
+            var button = $(event.relatedTarget);
+            var item_id = button.data('item_id');
+            var modal = $(this);
+            modal.find('.modal-content #registro_id').val(item_id);
+    });
+    </script>
+@endsection
+
+@section('content')
+
+<!-- Page Content -->
+<div class="content content-full content-boxed">
+    <!-- Partial Table -->
+    <div class="block block-rounded block-bordered">
+        <div class="block-header block-header-default">
+            <a class="btn btn-light" href="{{ URL::to('cms/inicio') }}">
+                <i class="fa fa-arrow-left mr-1"></i> Volver
+            </a>
+            <div class="block-options">
+                <button type="button" class="btn-block-option mr-2"><a href="{{ URL::to('cms/articulo/nuevo') }}"><i class="fa fa-plus mr-1"></i> Nuevo</a></button>
+                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="fullscreen_toggle"></button>
+                <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
+                    <i class="si si-refresh"></i>
+                </button>
+            </div>
+        </div>
+        <div class="block-content">
+            
+        <form action="{{ url('/cms/articulo') }}" method="get">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <label>
+                        <select name="perPage" class="custom-select" value="{{ $perPage }}">
+                            @foreach(['5','10','20'] as $page)
+                            <option @if($page == $perPage) selected @endif value="{{ $page }}">{{ $page }}</option>
+                            @endforeach
+                        </select>
+                    </label>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="q" name="q" value="{{ $q }}" placeholder="Buscar...">
+                        <div class="input-group-append">
+                            <button type="submit" class="input-group-text">
+                                <i class="fa fa-fw fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        
+            <table class="table table-bordered table-striped table-vcenter">
+                <thead class="thead-light">
+                    <tr>
+                        <th class="text-center" style="width: 100px;">ID</th>
+                        <th>Descripcion</th>
+                        <th>Formato</th>
+                        <th>Estado</th>
+                        <th class="d-none d-md-table-cell text-center" style="width: 100px;">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($tab_articulo as $key => $value)
+                    <tr>
+                        <td class="font-w600">{{ $value->id }}</td>
+                        <td class="d-none d-sm-table-cell"><em class="text-muted">{{ $value->de_item }}</em></td>
+                        <td class="d-none d-sm-table-cell">{{ $value->de_item_formato }}</td>
+                        <td class="d-none d-sm-table-cell">
+                            @if ($value->in_activo == true)
+                            <a href="{{ url('/cms/articulo/despublicar').'/'. $value->id }}">
+                                <i class="fa fa-eye text-done mr-1"></i>
+                            </a>
+                            @else
+                            <a href="{{ url('/cms/articulo/publicar').'/'. $value->id }}">
+                                <i class="fa fa-eye-slash text-danger mr-1"></i>
+                            </a>
+                            @endif
+                        </td>
+                        <td class="text-center">
+                            <div class="btn-group">
+                                <a href="{{ url('/cms/articulo/editar').'/'. $value->id }}">
+                                    <button type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Editar">
+                                        <i class="fa fa-pencil-alt"></i>
+                                    </button>
+                                </a>
+                                <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" title="Borrar" data-target="#borrar" data-item_id="{{ $value->id }}" >
+                                    <i class="fa fa-times"></i>
+                                </button>
+                                @if ($value->id != $maximo)
+                                <button type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Subir" onclick="location.href='{{ url('/cms/articulo/arriba').'/'. $value->id }}'">
+                                    <i class="fa fa-arrow-alt-circle-up"></i>
+                                </button>
+                                @else
+                                <button type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Subir" disabled>
+                                    <i class="fa fa-arrow-alt-circle-up"></i>
+                                </button>
+                                @endif
+                                @if ($value->id != $minimo)
+                                <button type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Bajar" onclick="location.href='{{ url('/cms/articulo/abajo').'/'. $value->id }}'">
+                                    <i class="fa fa-arrow-alt-circle-down"></i>
+                                </button>
+                                @else
+                                <button type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Bajar" disabled>
+                                    <i class="fa fa-arrow-alt-circle-down"></i>
+                                </button>
+                                @endif
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            {{ $tab_articulo->appends(Request::only(['perPage','q']))->render() }}         
+
+        </div>
+    </div>
+    <!-- END Partial Table -->
+</div>
+<!-- END Page Content -->
+
+@endsection
